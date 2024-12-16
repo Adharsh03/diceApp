ae4953a9632738da24e5fa0a43762d4b38d38396
Adharsh <129039920+Adharsh03@users.noreply.github.com>
2024-12-16 17:00:39 +0530

    Add files via upload

---
+++ lib/gradient_container.dart
@@ -0,0 +1,56 @@
+import 'package:diceapp/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.topRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            color1,
+            color2,
+          ],
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller()
+      ),
+    );
+  }
+} 
+
+
+
+
+/*class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key,required this.colors});
+
+  final List<Color> colors;
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StyledText('HeLlo World'),
+      ),
+    );
+  }
+}
+
+
+*/
